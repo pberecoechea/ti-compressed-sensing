e3d2e4ee70aa30555b043968cf8fc57d5a112033
Baptiste PLAUT-AUBRY <baptisteplauta.cd@gmail.com>
2025-11-30 22:36:54 +0100

    Ajout des commentaires dans tous les fichiers

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,11 @@
 # CONSTANTES
-
 PATH = "Data/"
 
 # Imagette, longueur arete carré
+#On choisit (arbitrairement) N = 11 car 297 et 528 sont divisibles par 11, 
+#on peut donc créer des batchs de taille N x N sans perdre d'information sur l'image 
 N = 11
 
-# dimensions image requise
+# dimensions image requise (dimensions de toutes les photos du dossier images)
 HAUTEUR = 297
 LARGEUR = 528
